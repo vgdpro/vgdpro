1746594705a926bc3ca0f4fcc0055476980389e5
I-linmeng-I <1364876516@qq.com>
2024-03-24 16:48:48 +0800

    更新hover的等级显示

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -1654,7 +1654,7 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 								if(!(mcard->type & TYPE_LINK)) {
 									const wchar_t* form = L"\u2605";
 									if (mcard->rank) form = L"\u2606";
-									myswprintf(formatBuffer, L"\n%ls%d", form, (mcard->level ? mcard->level : mcard->rank));
+									myswprintf(formatBuffer, L"\n%ls%d", form, (mcard->level ? mcard->level-1 : mcard->rank));
 									str.append(formatBuffer);
 								} else {
 									myswprintf(formatBuffer, L"\nLINK-%d", mcard->link);
